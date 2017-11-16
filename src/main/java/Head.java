bd41821323047a8933f80c5783a361cb7c12a9d2
Toggle <vjattich@bk.ru>
2017-11-16 20:15:34 +0500

    zzz

--- src/main/java/Head.java
+++ src/main/java/Head.java
@@ -1,13 +1,12 @@
 import org.telegram.telegrambots.api.objects.Update;
 
 public class Head extends Body {
-
     public void onUpdateReceived(Update update) {
-        if (update.getMessage().hasText() && update.hasMessage() && update.getMessage().getText().matches(KANA)) {
+        if (update.getMessage().hasText() && update.hasMessage()
+                && update.getMessage().getText().matches(KANA)) {
             sendMessage(translate("true"), update);
         } else {
             sendMessage(translate("false"), update);
         }
     }
-
 }
