630627ca9c84480ca8a87845911c87a35efa9d15
Toggle <vjattich@bk.ru>
2017-11-19 15:01:10 +0500

    a

--- src/main/java/Body.java
+++ src/main/java/Body.java
@@ -1,57 +1,34 @@
 
-import com.memetix.mst.language.Language;
-import com.memetix.mst.translate.Translate;
-import org.jsoup.Connection;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 abstract class Body extends Down {
 
-    //protected static Connection connection = Jsoup.connect("https://translate.google.com/#ja/ru");
-
-
-/*    protected String translate(String text) {
-        String translatedText = null;
-        try {
-            translatedText = Translate.execute(text, Language.JAPANESE, Language.RUSSIAN);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return translatedText;
-    }*/
-
     protected String translate(String text) {
-        String result = null;
-        StringBuffer url = new StringBuffer("https://translate.google.com/ja/ru/");
+        String result;
+        StringBuffer url = new StringBuffer("https://translate.google.com/#ja/ru");
+        url.append("/" + text);
         try {
-            Document doc = Jsoup.connect(String.valueOf(url)).get();
-            Element texting = doc.select("span[class=\"gt-ct-text\"]").first();
-            texting.appendText(text);
-            result = doc.select("span[class = \"\" ]").text();
+            Document doc = Jsoup.connect(String.valueOf(url)).userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36").get();
+            result = doc.title();
             System.out.println(result);
+            return result;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result == null ? "false" : result;
+        return "false";
     }
 
     protected void sendMessage(String text, Update update) {
         SendMessage message = new SendMessage()
-        .setChatId(update.getMessage().getChatId())
-        .setText(text);
+                .setChatId(update.getMessage().getChatId())
+                .setText(text);
         try {
             sendMessage(message);
         } catch (TelegramApiException e) {
